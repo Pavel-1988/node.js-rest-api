6960609ad98b1219d4ba7304ae1a7b2ffd52d579
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-03-05 23:08:46 +0200

    fix2

--- routes/api/contactsRouters.js
+++ routes/api/contactsRouters.js
@@ -10,9 +10,6 @@ const {
 const { isValidId, auth } = require('../../middlewares/')
 
 
-// const { addContactValidate, updateContactValidate, patchFavoriteValidate } = require('../../middlewares/contactsValidateSchema');
-// const {addContactSchema, updateContactSchema, favoriteContactSchema} = require('../../models/contactModel')
-
 const express = require('express')
 const router = express.Router()
 
@@ -21,14 +18,12 @@ router.get('/', auth, getAll )
 router.get('/:id',  auth, isValidId, getById)
 
 router.post('/', auth, add);
-// router.post('/', addContactValidate(addContactSchema), add);
 
 router.delete('/:id',auth, isValidId, deleteById )
 
 router.put('/:id', isValidId, updateById)
-// router.put('/:id', isValidId, updateContactValidate(updateContactSchema), updateById)
 
 router.patch('/:id/favorite', patchFavorite)
-// router.patch('/:id/favorite', patchFavoriteValidate(favoriteContactSchema), patchFavorite )
+
 
 module.exports = router
